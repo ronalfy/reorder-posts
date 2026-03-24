aa6d782e526cf8a5c279857448198e8259ce878a
Ronald Huereca <ronald@mediaron.com>
2026-03-23 21:37:15 -0500

    Fixing formatting.

--- uninstall.php
+++ uninstall.php
@@ -1,6 +1,5 @@
 <?php
-if ( !defined('ABSPATH') && !defined('WP_UNINSTALL_PLUGIN') ) {
-    exit();
+if ( ! defined( 'ABSPATH' ) && ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit();
 }
 delete_option( 'metronet-reorder-posts' );
-?>
